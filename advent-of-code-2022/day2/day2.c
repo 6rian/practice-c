e688f577a8e8d2d5583387726ce0781472eae6e2
Brian Griffin <wbgriffin@pm.me>
2022-12-02 17:50:39 -0500

    feat: add day2 part2 solution

--- advent-of-code-2022/day2/day2.c
+++ advent-of-code-2022/day2/day2.c
@@ -4,6 +4,12 @@
 
 #define MAX_LINE_SIZE 5
 
+enum point_values {
+  LOSE = 0,
+  DRAW = 3,
+  WIN = 6
+};
+
 FILE *open_file(char *filename) {
   FILE *pFile = fopen(filename, "r");
   if (!pFile) {
@@ -67,11 +73,42 @@ int solve(char *input_file) {
     my_score += points(my_play);
 
     if (is_draw(my_play, their_play)) {
-      my_score += 3;
+      my_score += DRAW;
       continue;
     }
 
-    if (my_win(my_play, their_play)) my_score += 6;
+    if (my_win(my_play, their_play)) my_score += WIN;
+  }
+
+  fclose(pFile);
+  return my_score;
+}
+
+int solve_part2(char *input_file) {
+  FILE *pFile = open_file(input_file);
+  int my_score = 0;
+  char buffer[MAX_LINE_SIZE];
+
+  while (fgets(buffer, MAX_LINE_SIZE, pFile) != NULL) {
+    char their_play = buffer[0];
+    char desired_outcome = buffer[2];
+
+    switch (their_play) {
+      case 'A': // rock
+        if (desired_outcome == 'X') my_score += LOSE + points('Z');
+        else if (desired_outcome == 'Y') my_score += DRAW + points('X');
+        else my_score += WIN + points('Y');
+        break;
+      case 'B': // paper
+        if (desired_outcome == 'X') my_score += LOSE + points('X');
+        else if (desired_outcome == 'Y') my_score += DRAW + points('Y');
+        else my_score += WIN + points('Z');
+        break;
+      case 'C': // scissors
+        if (desired_outcome == 'X') my_score += LOSE + points('Y');
+        else if (desired_outcome == 'Y') my_score += DRAW + points('Z');
+        else my_score += WIN + points('X');
+    }
   }
 
   fclose(pFile);
@@ -86,6 +123,9 @@ int main(int argc, char **argv) {
 
   int answer = solve(argv[1]);
   printf("My score is: %d\n", answer);
+
+  int answer2 = solve_part2(argv[1]);
+  printf("My desired score is: %d\n", answer2);
 
   return EXIT_SUCCESS;
 }
