bbae3eb5bcc4f70407eeb641e38078db08cba852
Brian Griffin <wbgriffin@pm.me>
2022-12-01 22:19:41 -0500

    feat: add day1 part2 solution

--- advent-of-code-2022/day1/day1.c
+++ advent-of-code-2022/day1/day1.c
@@ -22,11 +22,57 @@ int solve(char *input_file) {
     }
   }
 
-  // check if the last elf is carried the most
+  // check if the last elf is carrying the most
   if (sum > max) max = sum;
 
   fclose(pFile);
   return max;
+}
+
+int compare(const void *a, const void *b) {
+    return (*(int *)b - *(int *)a);
+}
+
+int solve_part2(char *input_file) {
+  FILE *pFile = fopen(input_file, "r");
+  if (!pFile) {
+    perror("[ERROR]");
+    exit(EXIT_FAILURE);
+  }
+
+  char buffer[10];
+  int sum = 0;
+  int max_sums_length = 2;
+  int *sums = (int *)malloc(max_sums_length * sizeof(int));
+  int count = 0;
+
+  while (fgets(buffer, 10, pFile) != NULL) {
+    if (strcmp(buffer, "\n") == 0) {
+      if (count == max_sums_length - 1) {
+        max_sums_length += 10;
+        sums = (int *)realloc(sums, max_sums_length * sizeof(int));
+      }
+      sums[count++] = sum;
+      sum = 0;
+    } else {
+      int calories = atoi(buffer);
+      sum += calories;
+    }
+  }
+
+  // don't forget about the last elf
+  if(count == max_sums_length -1) {
+    max_sums_length += 1;
+    sums = (int *)realloc(sums, max_sums_length * sizeof(int));
+  }
+  sums[count++] = sum;
+  
+  fclose(pFile);
+  qsort(sums, count, sizeof(int), compare);
+
+  int sum_top3 = sums[0] + sums[1] + sums[2];
+  free(sums);
+  return sum_top3;
 }
 
 int main(int argc, char **argv) {
@@ -36,7 +82,10 @@ int main(int argc, char **argv) {
   }
 
   int answer = solve(argv[1]);
-  printf("The elf carrying the most is carrying %d calories.\n\n", answer);
+  printf("The elf carrying the most is carrying %d calories.\n", answer);
+
+  int answer2 = solve_part2(argv[1]);
+  printf("The top 3 elves are carrying a total of %d calories.\n", answer2);
 
   return 0;
 }
