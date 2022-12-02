af4785f025a7e9f1d0a49f489668fe7da25aec10
Brian Griffin <wbgriffin@pm.me>
2022-12-01 21:04:24 -0500

    feat: finish day1 part1 solution

--- advent-of-code-2022/day1/day1.c
+++ advent-of-code-2022/day1/day1.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// int answer(char *input_file);
+#include <string.h>
 
 int solve(char *input_file) {
-  puts(input_file);
-  return 0;
+  FILE *pFile = fopen(input_file, "r");
+  if (!pFile) {
+    perror("[ERROR]");
+    exit(EXIT_FAILURE);
+  }
+
+  char buffer[10];
+  int sum = 0, max = 0;
+  while (fgets(buffer, 10, pFile) != NULL) {
+    printf("buffer=%s", buffer);
+    if (strcmp(buffer, "\n") == 0) {
+      printf("empty line");
+      if (sum > max) max = sum;
+      sum = 0;
+    } else {
+      int calories = atoi(buffer);
+      sum += calories;
+    }
+    printf("sum=%d, max=%d\n", sum, max);
+  }
+
+  // check if the last elf is carried the most
+  if (sum > max) max = sum;
+
+  fclose(pFile);
+  return max;
 }
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    puts("Error: please specify an input file.");
+    puts("[ERROR]: Please specify an input file.");
+    exit(EXIT_FAILURE);
   }
 
   int answer = solve(argv[1]);
