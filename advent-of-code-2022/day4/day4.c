d6ef5e9f9eccec3e345f4dadc49e919cd47f55d8
Brian Griffin <wbgriffin@pm.me>
2022-12-04 11:42:40 -0500

    refactor: prepare for part2

--- advent-of-code-2022/day4/day4.c
+++ advent-of-code-2022/day4/day4.c
@@ -18,7 +18,7 @@ bool in_set(uint16_t n, uint16_t min, uint16_t max) {
   return (n >= min && n <= max) ? true : false;
 }
 
-bool has_overlap(uint16_t* sections) {
+bool has_full_overlap(uint16_t* sections) {
   short a = sections[0];
   short b = sections[1];
   short c = sections[2];
@@ -28,19 +28,22 @@ bool has_overlap(uint16_t* sections) {
   return false;
 }
 
-uint16_t solve(char* input_file) {
+void solve(
+  char* input_file,
+  uint16_t* full_overlap,
+  uint16_t* partial_overlap
+) {
   FILE* pFile = open_file(input_file);
-  int overlap = 0;
   char line[MAX_LINE_SIZE];
 
   while (fgets(line, MAX_LINE_SIZE, pFile) != NULL) {
     uint16_t sections[4];
     parse_line(line, sections);
-    if (has_overlap(sections)) overlap += 1;
+    if (has_full_overlap(sections)) *full_overlap += 1;
   }
 
+  *partial_overlap = 1; // temporary
   fclose(pFile);
-  return overlap;
 }
 
 int main(int argc, char** argv) {
@@ -49,7 +52,11 @@ int main(int argc, char** argv) {
     exit(EXIT_FAILURE);
   }
 
-  printf("Part1 answer: %d\n", solve(argv[1]));
-  // printf("Part2 answer: %d\n", solve_part2(argv[1]));
+  uint16_t part_1 = 0;
+  uint16_t part_2 = 0;
+  solve(argv[1], &part_1, &part_2);
+
+  printf("Part1 answer: %d\n", part_1);
+  printf("Part2 answer: %d\n", part_2);
   return EXIT_SUCCESS;
 }
